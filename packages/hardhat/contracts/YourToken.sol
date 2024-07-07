65ea5b678851c08fd19887b0a149afd0f2145724
Volkan Güneri <87070090+volkanguneri@users.noreply.github.com>
2024-07-07 14:35:11 +0200

    Update YourToken.sol
    finished yourToken.sol

--- packages/hardhat/contracts/YourToken.sol
+++ packages/hardhat/contracts/YourToken.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 // learn more: https://docs.openzeppelin.com/contracts/4.x/erc20
 
 contract YourToken is ERC20 {
-  constructor() ERC20("Gold", "GLD") {
-    //_mint( ~~~YOUR FRONTEND ADDRESS HERE~~~~ , 1000 * 10 ** 18);
-  }
+	constructor() ERC20("Gold", "GLD") {
+		_mint(msg.sender, 2000 * 10 ** 18);
+	}
 }
